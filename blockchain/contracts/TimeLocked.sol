e9e5e99fd88a9a5a8d3fbea5a57a9bda58e7e19f
Alexandru Matei <alexandrumatei3693@gmail.com>
2021-05-29 19:46:04 +0300

    Complete contract base functions

--- blockchain/contracts/TimeLocked.sol
+++ blockchain/contracts/TimeLocked.sol
@@ -15,8 +15,23 @@ contract TimeLocked is Ownable {
 
     function deposit(address tokenAddress, address unlocker, uint256 amount, uint unlockTimestamp) public onlyOwner {
         require(amount > 0, "Amount must be > 0");
+
         IERC20 token = IERC20(tokenAddress);
-        deposits[unlocker][tokenAddress] = Deposit(tokenAddress, amount, unlockTimestamp);
+        Deposit storage d = deposits[unlocker][tokenAddress];
+        d.token = tokenAddress;
+        d.amount = amount;
+        d.unlockTimestamp = unlockTimestamp;
+
         token.transferFrom(msg.sender, address(this), amount);
+    }
+
+    function claim(address tokenAddress) public {
+        Deposit storage d = deposits[msg.sender][tokenAddress];
+        require(d.token != address(0), "No funds to claim");
+        require(block.timestamp > d.unlockTimestamp, "Can't claim locked tokens");
+        uint256 amount = d.amount;
+        delete deposits[msg.sender][tokenAddress];
+        IERC20 token = IERC20(tokenAddress);
+        token.transfer(msg.sender, amount);
     }
 }
--- blockchain/test/time-locked.test.ts
+++ blockchain/test/time-locked.test.ts
@@ -19,20 +19,49 @@ describe("TimeLocked contract", () => {
 
     it("should deposit tokens", async () => {
         await token.approve(timeLocked.address, ethers.utils.parseEther("1"));
-        const timestamp = Math.floor(Date.now() / 1000) + 10;
-        await timeLocked.deposit(token.address, await accounts[1].getAddress(), ethers.utils.parseEther("1"), timestamp)
+        await timeLocked.deposit(
+            token.address, 
+            await accounts[1].getAddress(), 
+            ethers.utils.parseEther("1"),
+            Math.floor(Date.now() / 1000) + 10
+        );
         const amount = (await timeLocked.deposits(await accounts[1].getAddress(), token.address)).amount
         expect(amount).to.eq(ethers.utils.parseEther("1"));
     })
 
     it("should not deposit 0 amount", async () => {
         await token.approve(timeLocked.address, ethers.utils.parseEther("1"));
-        const timestamp = Math.floor(Date.now() / 1000) + 10;
         await expect(timeLocked.deposit(
             token.address, 
             await accounts[1].getAddress(), 
             ethers.utils.parseEther("0"), 
-            timestamp.toString()
+            Math.floor(Date.now() / 1000) + 10
         )).to.be.revertedWith("Amount must be > 0")
     })
+
+    it("should reject if claimer has no tokens", async () => {
+        await expect(timeLocked.connect(accounts[2]).claim(token.address)).to.be.revertedWith("No funds to claim");
+    })
+
+    it("should reject if tokens are locked", async () => {
+        await expect(timeLocked.connect(accounts[1]).claim(token.address)).to.be.revertedWith("Can't claim locked tokens");
+    })
+
+    it("should claim tokens", async() => {
+        await wait(10);
+        await timeLocked.connect(accounts[1]).claim(token.address);
+        const balance = (await token.balanceOf(await accounts[1].getAddress())).toString();
+        expect(balance).to.eq(ethers.utils.parseEther("1"));
+    })
+
+    it("should not be able to claim tokens twice", async () => {
+        await expect(timeLocked.connect(accounts[1]).claim(token.address)).to.be.revertedWith("No funds to claim");
+        const balance = (await token.balanceOf(await accounts[1].getAddress())).toString()
+    })
 })
+
+const wait = (seconds: number = 10) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, seconds * 1000)
+    })
+}
